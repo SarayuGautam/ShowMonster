98144de1179784b079b64e9762c6c55e4bacc262
Sarayu Gautam <sarayugautam1@gmail.com>
2020-03-28 09:09:42 +0545

    Updates

---
+++ precache-manifest.413c1bb63f94688568be01e50d61eebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dee9587c58d6f22165c9671040aa5d4",
+    "url": "/https//sarayugautam.github.io/Monster-Rolodex-react/index.html"
+  },
+  {
+    "revision": "99d676ee46453606c06a",
+    "url": "/https//sarayugautam.github.io/Monster-Rolodex-react/static/css/main.0f8ab20e.chunk.css"
+  },
+  {
+    "revision": "4b5eecc8adbc1bb63d82",
+    "url": "/https//sarayugautam.github.io/Monster-Rolodex-react/static/js/2.5da29e99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//sarayugautam.github.io/Monster-Rolodex-react/static/js/2.5da29e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d676ee46453606c06a",
+    "url": "/https//sarayugautam.github.io/Monster-Rolodex-react/static/js/main.1bc6254c.chunk.js"
+  },
+  {
+    "revision": "54a44b8d47170ef6aa37",
+    "url": "/https//sarayugautam.github.io/Monster-Rolodex-react/static/js/runtime-main.9a771a93.js"
+  }
+]);
